9bd088909b0e4c53bf7a546d6892fea152b5a42d
ejhall04 <halle14@nku.edu>
2023-04-22 19:05:21 -0400

    Finishing touches

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ let userInput = ['test']
 
 app.get('/', (req, res) => {
   res.send('Hello World! ' + req.statusCode)
+  console.log('Database updated')
 })
 app.post('/', (req, res) => {
     let data = req.body;
     res.send('Got a POST request ' + req.statusCode)
+    console.log('Database updated')
  })
 app.put('/user', (req, res) => {
     res.send('Got a PUT request at /user ' + req.statusCode)
+    console.log('Database updated')
   })
 app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at /user ' + req.statusCode)
+    console.log('Database updated')
   })
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
